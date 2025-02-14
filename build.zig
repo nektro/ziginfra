d3a3b59df48c7cf996c1ce5e9b37672a97096db3
Meghan Denny <hello@nektro.net>
2025-02-14 03:54:23 -0800

    ensure step name is package name

--- build.zig
+++ build.zig
@@ -5,40 +5,50 @@ pub fn build(b: *std.Build) void {
     const mode = b.option(std.builtin.Mode, "mode", "") orelse .Debug;
 
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&b.dependency("arqv-ini", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("gimme", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-ansi", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-bencode", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-color", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-detect-license", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-expect", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-extras", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-flag", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-fmt-valueliteral", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-git", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-iana-tlds", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-icu", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-inquirer", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-intrusive-parser", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-iso-3166-countrys", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-iso-639-languages", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    // test_step.dependOn(&b.dependency("zig-js", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step); // not passing 100%
-    test_step.dependOn(&b.dependency("zig-json", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-leven", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-licenses", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-licenses-text", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-mime", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-oauth2", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-pek", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-signal", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-time", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-tracer", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-ulid", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-unicode-uca", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-unicode-ucd", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-UrlValues", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    // test_step.dependOn(&b.dependency("zig-webidl", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step); // not passing 100%
-    test_step.dependOn(&b.dependency("zig-xml", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-yaml", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
-    test_step.dependOn(&b.dependency("zig-zorm", .{ .target = target, .mode = mode }).builder.top_level_steps.get("test").?.step);
+    test_step.dependOn(dependOn(b, "arqv-ini", target, mode));
+    test_step.dependOn(dependOn(b, "gimme", target, mode));
+    test_step.dependOn(dependOn(b, "zig-ansi", target, mode));
+    test_step.dependOn(dependOn(b, "zig-bencode", target, mode));
+    test_step.dependOn(dependOn(b, "zig-color", target, mode));
+    test_step.dependOn(dependOn(b, "zig-detect-license", target, mode));
+    test_step.dependOn(dependOn(b, "zig-expect", target, mode));
+    test_step.dependOn(dependOn(b, "zig-extras", target, mode));
+    test_step.dependOn(dependOn(b, "zig-flag", target, mode));
+    test_step.dependOn(dependOn(b, "zig-fmt-valueliteral", target, mode));
+    test_step.dependOn(dependOn(b, "zig-git", target, mode));
+    test_step.dependOn(dependOn(b, "zig-iana-tlds", target, mode));
+    test_step.dependOn(dependOn(b, "zig-icu", target, mode));
+    test_step.dependOn(dependOn(b, "zig-inquirer", target, mode));
+    test_step.dependOn(dependOn(b, "zig-intrusive-parser", target, mode));
+    test_step.dependOn(dependOn(b, "zig-iso-3166-countrys", target, mode));
+    test_step.dependOn(dependOn(b, "zig-iso-639-languages", target, mode));
+    // test_step.dependOn(dependOn(b,"zig-js", target,mode)); // not passing 100%
+    test_step.dependOn(dependOn(b, "zig-json", target, mode));
+    test_step.dependOn(dependOn(b, "zig-leven", target, mode));
+    test_step.dependOn(dependOn(b, "zig-licenses", target, mode));
+    test_step.dependOn(dependOn(b, "zig-licenses-text", target, mode));
+    test_step.dependOn(dependOn(b, "zig-mime", target, mode));
+    test_step.dependOn(dependOn(b, "zig-oauth2", target, mode));
+    test_step.dependOn(dependOn(b, "zig-pek", target, mode));
+    test_step.dependOn(dependOn(b, "zig-signal", target, mode));
+    test_step.dependOn(dependOn(b, "zig-time", target, mode));
+    test_step.dependOn(dependOn(b, "zig-tracer", target, mode));
+    test_step.dependOn(dependOn(b, "zig-ulid", target, mode));
+    test_step.dependOn(dependOn(b, "zig-unicode-uca", target, mode));
+    test_step.dependOn(dependOn(b, "zig-unicode-ucd", target, mode));
+    test_step.dependOn(dependOn(b, "zig-UrlValues", target, mode));
+    // test_step.dependOn(dependOn(b,"zig-webidl", target,mode)); // not passing 100%
+    test_step.dependOn(dependOn(b, "zig-xml", target, mode));
+    test_step.dependOn(dependOn(b, "zig-yaml", target, mode));
+    test_step.dependOn(dependOn(b, "zig-zorm", target, mode));
+}
+
+fn dependOn(b: *std.Build, name: []const u8, target: std.Build.ResolvedTarget, mode: std.builtin.OptimizeMode) *std.Build.Step {
+    const dependency = b.dependency(name, .{
+        .target = target,
+        .mode = mode,
+    });
+    const step = &dependency.builder.top_level_steps.get("test").?.step;
+    step.name = name;
+    return step;
 }
