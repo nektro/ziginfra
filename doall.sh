449006630ce8d3bae886592a567b56443fd04e9d
Meghan Denny <hello@nektro.net>
2025-05-10 15:13:48 -0700

    doall: add collapse groups in github actions

--- doall.sh
+++ doall.sh
@@ -5,9 +5,11 @@ pwd=$PWD
 
 for x in $(ls .zigmod/deps/git/github.com/nektro)
 do
+    [ "$GITHUB_ACTIONS" = "true" ] && echo ::group::$x
     path=".zigmod/deps/git/github.com/nektro/$x"
     echo $path
     cd $path
     $@
+    [ "$GITHUB_ACTIONS" = "true" ] && echo ::endgroup::
     cd $pwd
 done
